4634d25f385c0aa5d689b79252bde6f65fed8389
Rskul <richardsku@gmail.com>
2024-03-26 15:03:20 +0100

    Update DnDDiceRoller.py

---
+++ DnDDiceRoller.py
@@ -0,0 +1,8 @@
+#goal: simple program that allow me to roll for multiple monsters at once and adds relevant stats
+    #plan:
+        #Load monsters from text file. Be able to edit the file on fly as the program is running
+        #Save any number of monster in code including relevatn stats. (monsters as objects?)
+        #Input to determine relevant stat and roll difficulty.
+        #Output Name of monster, roll + modifier, pass/fail, crit (nat 1 or 20)
+        #Order monsters in order of iniciative
+            #Keep the order even when adding/removing monsters from the text file
